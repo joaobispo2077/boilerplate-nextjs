4e027dcafbe9cbc3ec4a2dc17df8fea1328f4a74
João Bispo <joaobispo2077@gmail.com>
2021-08-06 16:25:38 -0300

    refactor: change husky command from yarn to use npm

--- src/components/CloneMe/CloneMe.spec.tsx
+++ src/components/CloneMe/CloneMe.spec.tsx
@@ -4,6 +4,7 @@ import { screen, render } from '@testing-library/react';
 
 describe('<CloneMe/ >', () => {
   it('should render', () => {
+    // to render
     render(renderTheme(<CloneMe title="CloneMe" />));
     expect(screen.getByRole('heading')).toBeInTheDocument();
   });
