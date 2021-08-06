695dcad4271203d21355691a074f6c1de832cf48
João Bispo <joaobispo2077@gmail.com>
2021-08-06 18:23:54 -0300

    feat: add default theme into app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import { ThemeProvider } from 'styled-components';
 import GloblaStyles from '../styles/GloblaStyles';
-import { themes } from '../styles/themes/index';
+import { theme } from '../styles/themes/index';
 import { AppProps } from 'next/app';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ThemeProvider theme={{ ...themes['default'] }}>
+      <ThemeProvider theme={theme}>
         <GloblaStyles />
         <Component {...pageProps} />
       </ThemeProvider>
